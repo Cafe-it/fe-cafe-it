9d3537c7456105ec2509adcdb7d6b408643112d7
CodyMan0 <hys83751952@gmail.com>
2025-07-03 18:00:16 +0900

    [FEAT] 깔끔한 pretendard 폰트로 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
+import { pretendard } from "./fonts";
 
 export const metadata: Metadata = {
   title: "Cafe-it",
@@ -13,8 +14,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+    <html lang="ko">
+      <body className={`${pretendard.variable} font-pretendard antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
